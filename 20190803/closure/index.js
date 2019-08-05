61daeade208ce13a1c9a0c095e51e49fc9a0e865
habc0807 <gao0807@foxmail.com>
2019-08-05 23:23:38 +0800

    feat: four

--- 20190803/closure/index.js
+++ 20190803/closure/index.js
@@ -119,41 +119,153 @@
  * 需要多张图片 图片预加载处理
  */
 
- (function () {
-     // 准备资源一个人开发
-     function prepare() {
-         const context = document.getElementById('content').getContext('2d')
-         const heroImg = new Image()
-         const allSpriteImg = new Image()
+//  (function () {
+//      // 准备资源一个人开发
+//      function prepare() {
+//          const context = document.getElementById('content').getContext('2d')
+//          const heroImg = new Image()
+//          const allSpriteImg = new Image()
 
-         const imgTask = (img, src) => {
-             return new Promise(function (resolve, reject) {
-                 img.onload = resolve 
-                 img.onerror = reject 
-                 img.src = src 
-             })
-         }
+//          const imgTask = (img, src) => {
+//              return new Promise(function (resolve, reject) {
+//                  img.onload = resolve 
+//                  img.onerror = reject 
+//                  img.src = src 
+//              })
+//          }
 
-         const allresouceTask = Promise.all([
-             imgTask(heroImg, './hero.png'),
-             imgTask(allSpriteImg, './all.jpg')
-         ])
+//          const allresouceTask = Promise.all([
+//              imgTask(heroImg, './hero.png'),
+//              imgTask(allSpriteImg, './all.jpg')
+//          ])
 
-         return {
-             /**
-              * 
-              * @param {Function} callback - 外部传入的callback 可以拿到 prepare内部的变量
-              */
-             getResource(callback) {
-                allresouceTask.then(function(res) { // 图片加载完成后 再画图 保证不会在加载完毕之前使用这个图片
+//          return {
+//              /**
+//               * 
+//               * @param {Function} callback - 外部传入的callback 可以拿到 prepare内部的变量
+//               */
+//              getResource(callback) {
+//                 allresouceTask.then(function(res) { // 图片加载完成后 再画图 保证不会在加载完毕之前使用这个图片
+//                     callback && callback(context, heroImg, allSpriteImg)
+//                 })
+//              }
+//          }
+//      }
+
+//     //  画图一个人开发
+//      function drawImageHandle(context, heroImg, allSpriteImg) {
+//         // draw 需要定义在hexo 和 monster之前 否则通过hexo.draw()的方式访问不到
+//         var draw = function () {
+//             console.log(this.context)
+//             this.context.drawImage(
+//                 this.img,
+//                 this.imgPos.x,
+//                 this.imgPos.y,
+//                 this.imgPos.width,
+//                 this.imgPos.height,
+//                 this.rect.x,
+//                 this.rect.y,
+//                 this.rect.width,
+//                 this.rect.height
+//             )
+//         }
+
+//         var hero = {
+//              img: heroImg,
+//              context: context,
+//              imgPos: {
+//                  x: 0,
+//                  y: 0,
+//                  width: 32,
+//                  height: 32
+//             },
+//             rect: {
+//                 x: 0,
+//                 y: 0,
+//                 width: 40,
+//                 height: 40
+//             },
+//             draw: draw
+//         }
+
+//         var monster = {
+//             img: allSpriteImg,
+//             context: context,
+//             imgPos: {
+//                 x: 858,
+//                 y: 529,
+//                 width: 32,
+//                 height: 32
+//             },
+//             rect: {
+//                 x: 100,
+//                 y: 100,
+//                 width: 40,
+//                 height: 40
+//             },
+//             draw: draw
+//         }
+
+        
+
+//         hero.draw()
+//         monster.draw()
+//      }
+
+//      var resourceManager = prepare()
+//      resourceManager.getResource(function (context, heroImg, allSpriteImg) {
+//         //  console.log(allSpriteImg)
+//          drawImageHandle(context, heroImg, allSpriteImg)
+//      })
+//  })()
+
+
+
+
+
+
+
+
+/**
+ * 第四版
+ * 用原型链继承的方法 添加两种怪兽
+ */
+
+(function () {
+    // 准备资源一个人开发
+    function prepare() {
+        const context = document.getElementById('content').getContext('2d')
+        const heroImg = new Image()
+        const allSpriteImg = new Image()
+
+        const imgTask = (img, src) => {
+            return new Promise(function (resolve, reject) {
+                img.onload = resolve
+                img.onerror = reject
+                img.src = src
+            })
+        }
+
+        const allresouceTask = Promise.all([
+            imgTask(heroImg, './hero.png'),
+            imgTask(allSpriteImg, './all.jpg')
+        ])
+
+        return {
+            /**
+             * 
+             * @param {Function} callback - 外部传入的callback 可以拿到 prepare内部的变量
+             */
+            getResource(callback) {
+                allresouceTask.then(function (res) { // 图片加载完成后 再画图 保证不会在加载完毕之前使用这个图片
                     callback && callback(context, heroImg, allSpriteImg)
                 })
-             }
-         }
-     }
+            }
+        }
+    }
 
     //  画图一个人开发
-     function drawImageHandle(context, heroImg, allSpriteImg) {
+    function drawImageHandle(context, heroImg, allSpriteImg) {
         // draw 需要定义在hexo 和 monster之前 否则通过hexo.draw()的方式访问不到
         var draw = function () {
             console.log(this.context)
@@ -171,13 +283,13 @@
         }
 
         var hero = {
-             img: heroImg,
-             context: context,
-             imgPos: {
-                 x: 0,
-                 y: 0,
-                 width: 32,
-                 height: 32
+            img: heroImg,
+            context: context,
+            imgPos: {
+                x: 0,
+                y: 0,
+                width: 32,
+                height: 32
             },
             rect: {
                 x: 0,
@@ -206,15 +318,15 @@
             draw: draw
         }
 
-        
+
 
         hero.draw()
         monster.draw()
-     }
+    }
 
-     var resourceManager = prepare()
-     resourceManager.getResource(function (context, heroImg, allSpriteImg) {
+    var resourceManager = prepare()
+    resourceManager.getResource(function (context, heroImg, allSpriteImg) {
         //  console.log(allSpriteImg)
-         drawImageHandle(context, heroImg, allSpriteImg)
-     })
- })()
+        drawImageHandle(context, heroImg, allSpriteImg)
+    })
+})()
