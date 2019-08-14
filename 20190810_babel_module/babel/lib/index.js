e85c99aa7bdb8a7acc7e4afbb4d3b7ebbecd9c6c
habc0807 <gao0807@foxmail.com>
2019-08-14 19:02:17 +0800

    add .gitignore

---
+++ 20190810_babel_module/babel/lib/index.js
@@ -0,0 +1,8 @@
+"use strict";
+
+var _a = _interopRequireDefault(require("./a"));
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
+
+console.log(_a["default"]);
+console.log((0, _a["default"])()); // console.log(JSON.stringify(obj()))
---
+++ 20190810_babel_module/babel/note.md
@@ -0,0 +1,3 @@
+### 如果只用babel编译 （不使用webpack）
+
+./node_modules/.bin/babel src --out-dir lib 
