d241896bf68f156e7987b461a10c4cc078a8e5d5
habc0807 <gao0807@foxmail.com>
2019-08-14 19:01:18 +0800

    babel

--- 20190810/b.js
+++
@@ -1,6 +0,0 @@
-var a = require('./a.js') // require出来的回是一个单例 
-// console.log('a: ' + JSON.stringify(a) )
-
-console.log('b: ' + a.getName())
-a.setName('jingzhang')
-console.log('b-2: ' + a.getName())
