5c81e404031d0cb1651efbfb85fb30cbdbd82be0
habc0807 <gao0807@foxmail.com>
2019-08-21 18:15:54 +0800

    pic list

--- 20190817_vue_toutiao/src/pages/main.vue
+++ 20190817_vue_toutiao/src/pages/main.vue
@@ -1,7 +1,24 @@
 <template>
     <div>
-        <div v-for="(item, key) in list" :key="key">单图{{key}}</div>
-        <!-- <div>多图</div> -->
+        <div v-for="(item, key) in list" :key="key">
+            <!-- 单图 -->
+            <div v-if="item.type == 'singlePic'" class="item single-pic">
+                <div class="content">
+                    <span>
+                        {{ item.data.title }}
+                    </span>
+                </div>
+                <img v-bind:src="item.data.imageList[0]" alt="">
+            </div>
+
+            <!-- 组件 -->
+            <div v-else-if="item.type == 'multiplePic'" class="item multiple-image">
+                <h3>{{ item.data.title }}</h3>
+                <div class="image-list">
+                    <img v-for="(image, index) in item.data.imageList" :key="index" v-bind:src="image" alt="">
+                </div>
+            </div>
+        </div>
     </div>
 </template>
 
@@ -31,5 +48,39 @@ export default {
 }
 </script>
 <style lang="css">
-    
+body {
+    margin: 0;
+    padding: 0;
+}
+
+.item {
+    min-height: 42px;
+    padding: 16px 0;
+    margin: 0 15px;
+    border-bottom: 1px solid rgba(221, 221, 221, 0.6);
+}
+
+.item.single-pic {
+    display: flex;
+}
+
+.single-pic .content {
+    flex: 2;
+    font-size: 17px;
+    padding-right: 24px;
+}
+
+.single-pic img {
+    width: 33%;
+    display: block;
+}
+
+.item.multiple-image h3 {
+    font-size: 17px;
+}
+
+.item.multiple-image .image-list img {
+    width: 33%;
+}
+
 </style>
